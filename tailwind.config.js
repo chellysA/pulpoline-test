35d0b80c82fb1374923c831c737ba5d017ac01f7
chellysA <chellys.castillo@gmail.com>
2024-05-01 19:21:37 -0400

    feat: :lipstick: add new colors at tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,9 +14,15 @@ export default {
       },
       colors: {
         primary: "#F85900",
+        yellow: "#F8C100",
+        orange: "#FECB47",
         grey: "#B9B9B9",
+        grey100: "#F4F5F8",
         softGrey: "#CACACA",
         lightGrey: "#EEEEEE",
+      },
+      images: {
+        cutlery: "url('img/Cutlery.png')",
       },
     },
   },
