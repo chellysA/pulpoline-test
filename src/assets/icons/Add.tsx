7fe9e21cd7e089716e23fbfc31a847cf9fee14a9
chellysA <chellys.castillo@gmail.com>
2024-05-02 20:00:07 -0400

    style: :lipstick: add images and icons shopping

---
+++ src/assets/icons/Add.tsx
@@ -0,0 +1,16 @@
+const Add = () => (
+  <svg
+    width={33}
+    height={34}
+    viewBox="0 0 33 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y={0.217773} width={33} height={33} rx={16.5} fill="#F85900" />
+    <path
+      d="M15.5714 17.6463H10V15.7892H15.5714V10.2178H17.4286V15.7892H23V17.6463H17.4286V23.2178H15.5714V17.6463Z"
+      fill="white"
+    />
+  </svg>
+);
+export default Add;
---
+++ src/assets/icons/Substract.tsx
@@ -0,0 +1,16 @@
+const Substract = () => (
+  <svg
+    width={33}
+    height={34}
+    viewBox="0 0 33 34"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect y={0.217773} width={33} height={33} rx={16.5} fill="#CACACA" />
+    <path
+      d="M15.6429 17.6843H10.5V15.7178H15.6429L17.3571 15.7185L22.5 15.7178V17.6843H17.3571V17.7178L15.6429 17.6843Z"
+      fill="white"
+    />
+  </svg>
+);
+export default Substract;
